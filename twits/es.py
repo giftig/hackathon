1b7648a41ae35e9c1701634e4ceb2146f9c82348
Rob Moore <robert.moore@bglgroup.co.uk>
2018-09-06 11:50:24 +0100

    Set default ES properties to the AWS account

--- twits/es.py
+++ twits/es.py
@@ -5,8 +5,12 @@ from elasticsearch import Elasticsearch
 
 class ESClient(object):
     ES_PROTO = os.environ.get('ES_PROTO', 'https')
-    ES_HOST = os.environ.get('ES_HOST', 'localhost')
-    ES_PORT = int(os.environ.get('ES_PORT', '9200'))
+    ES_HOST = os.environ.get(
+        'ES_HOST',
+        'search-data-insight-p6k37focny45vkoxg62ikpduli.eu-west-1'
+        '.es.amazonaws.com'
+    )
+    ES_PORT = int(os.environ.get('ES_PORT', '443'))
     ES_INDEX = os.environ.get('ES_INDEX', 'twits')
 
     es = Elasticsearch('%s://%s:%s' % (ES_PROTO, ES_HOST, ES_PORT))
