16b5f9441ca280f857658cf4ddcf009cb1ee4b31
Rob Moore <robert.moore@bglgroup.co.uk>
2018-09-06 11:46:09 +0100

    Configure ES properly

--- twits/es.py
+++ twits/es.py
@@ -4,11 +4,12 @@ from elasticsearch import Elasticsearch
 
 
 class ESClient(object):
+    ES_PROTO = os.environ.get('ES_PROTO', 'https')
     ES_HOST = os.environ.get('ES_HOST', 'localhost')
     ES_PORT = int(os.environ.get('ES_PORT', '9200'))
     ES_INDEX = os.environ.get('ES_INDEX', 'twits')
 
-    es = Elasticsearch()
+    es = Elasticsearch('%s://%s:%s' % (ES_PROTO, ES_HOST, ES_PORT))
 
     def insert_entry(self, data):
-        self.es.index(index=self.ES_INDEX, doc_type='twit', body=data)
+        print self.es.index(index=self.ES_INDEX, doc_type='twit', body=data)
