1493b57846051eda8e023ebc7d3abdabc12a5673
Rob Moore <robert.moore@bglgroup.co.uk>
2018-09-06 13:13:56 +0100

    Fix mapping and flatten model

--- twits/modeller.py
+++ twits/modeller.py
@@ -25,11 +25,16 @@ class Modeller(object):
         created = datetime.datetime.utcfromtimestamp(ts // 1000)
         author_weighting = tweet.get('user', {}).get('followers_count', 0)
 
-        return {
+        data = {
             'author_weighting': author_weighting,
-            'emotion_analysis': self.analyser.analyse(text),
             'keywords': hashtags,
             'raw_text': text,
             'source': 'twitter',
             'timestamp': self._format_time(created)
         }
+        # Flattened for easier modelling
+        data.update({
+            'emotion_analysis.%s' % k: v
+            for k, v in self.analyser.analyse(text).iteritems()
+        })
+        return data
